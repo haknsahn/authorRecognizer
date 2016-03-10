cd63dd29e29e5a23e5c2dcfd33f07ce24cee7e48
Hakan Şahin <hakansahin993@gmail.com>
2016-03-10 17:41:00 +0200

    alfa version done.
    * Naive Bayes implemented.
    * Needs to be improved.

--- author.py
+++ author.py
@@ -7,6 +7,12 @@ class author:
 		self.name = name
 		self.document_list = []
 		self.vocabulary = {}
+		self.doc_count = 0
+		self.tot_token_count = 0
+
+	def compute_tot_token_count(self):
+		for count in self.vocabulary.itervalues():
+			self.tot_token_count += count
 
 	def add_document(self, id):
 		self.document_list.append(id)		
--- author_recognizer.py
+++ author_recognizer.py
@@ -3,6 +3,7 @@
 import sys, os 
 from author import author
 from document import document
+from fnmatch import filter
 
 # Read documents in given path, returns author and list of document objects.
 def read_documents(author_dir):
@@ -27,7 +28,34 @@ def construct_vocabulary(doc_dict):
 				vocabulary.setdefault(token, 0)				# Adds tokens to vocabulary.
 				vocabulary[token] += count
 		auth.vocabulary = auth_vocabulary
+		auth.compute_tot_token_count()
 	return vocabulary
+
+# Performs 'Naive Bayes' in order to determine which author has written the given text.
+def determine_author(text, training_doc_dict, vocabulary, tot_doc_count):
+	auth_res = {}
+	bow = document("","",text).construct_bow()
+	for auth in training_doc_dict.iterkeys():
+		auth_res[auth.name] = 1.0 * auth.doc_count / tot_doc_count
+		
+		cnt = 0
+		for token, count in bow.iteritems():
+			if cnt > 30:
+				break
+			cnt += 1
+			token_pos = (auth.vocabulary.get(token,0) + 1) / (1.0 * (auth.tot_token_count + len(vocabulary)))
+			auth_res[auth.name] *= token_pos ** count
+
+	sorted_auth_res = sorted(auth_res, key=auth_res.get)
+	return sorted_auth_res[-1]
+
+# Returns a list of all files in given directory.
+def traverse_dir(path):
+	doc_list = []
+	for root, dir, files in os.walk(path):
+		for items in filter(files, "*"):
+			doc_list.append(root + "/" + items)
+	return doc_list
 
 # Default paths.
 training_path = 'training_dataset/'
@@ -40,10 +68,21 @@ if __name__ == "__main__":
 		training_path 	= os.path.normpath(sys.argv[1]) + "/"
 		test_path 		= os.path.normpath(sys.argv[2]) + "/"
 
-
+	tot_doc_count = 0
 	training_doc_dict = {}
 	for author_dir in os.listdir(training_path):
 		(auth, doc_list) = read_documents(training_path + author_dir)
+		auth.doc_count = len(doc_list)
+		tot_doc_count += auth.doc_count
 		training_doc_dict[auth] = doc_list
 
 	vocabulary = construct_vocabulary(training_doc_dict)
+
+	test_doc_list = traverse_dir(test_path)
+	for doc_path in test_doc_list:
+		if doc_path.endswith('.txt'):
+			doc_text = open(doc_path,'r').read()
+			print os.path.basename(os.path.dirname(doc_path)) + " : " + determine_author(doc_text, training_doc_dict, vocabulary, tot_doc_count)
+		
+
+
